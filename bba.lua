40631b57f231038a1b7e7f7802ca5ab1a51c08f5
AOvVaw1ol0v <unknowsito7@gmail.com>
2025-04-13 11:32:23 -0500

    Update bba.lua

--- bba.lua
+++ bba.lua
@@ -1,1 +1,18 @@
-
+return {
+    HWIDs = {
+        "HWID1",
+        "HWID2",
+    },
+    IPs = {
+        "",
+        "",
+    },
+    UserIDs = {
+        123456789,
+        987654321,
+    },
+    Usernames = {
+        "BadUser1",
+        "ExploitUser",
+    }
+}
