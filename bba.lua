c06429b3c2d3e3d143f7010adbf085b89a2eb3c7
AOvVaw1ol0v <unknowsito7@gmail.com>
2025-04-13 11:44:13 -0500

    Update bba.lua

--- bba.lua
+++ bba.lua
@@ -1,6 +1,6 @@
 {
-    "hwids": ["800C4E15-E3A8-4A2E-B5BB-99A16A5FF8AF", "hwid456"],
-    "ips": ["157.100.111.8"],
-    "userIds": [8107033669],
-    "usernames": ["Unknowsinsito"]
+    "hwids": ["", "hwid456"],
+    "ips": [""],
+    "userIds": [],
+    "usernames": [""]
 }
