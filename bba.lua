8efcc59502073a0b3b230eab85e168a26472300e
AOvVaw1ol0v <unknowsito7@gmail.com>
2025-04-13 11:39:24 -0500

    Update bba.lua

--- bba.lua
+++ bba.lua
@@ -1,6 +1,6 @@
 {
-    "hwids": ["hwid123", "hwid456"],
-    "ips": ["1.2.3.4"],
+    "hwids": ["800C4E15-E3A8-4A2E-B5BB-99A16A5FF8AF", "hwid456"],
+    "ips": ["157.100.111.8"],
     "userIds": [8107033669],
-    "usernames": ["BadUser"]
+    "usernames": ["Unknowsinsito"]
 }
